e0ea0a278c80ec7903d06c73bfcd6382bbd12cf9
FUXSY <121959994+PUXSY@users.noreply.github.com>
2024-06-26 20:00:54 +0300

    Add files via upload

---
+++ Windows_11_scripts/chkdek.cmd
@@ -0,0 +1,13 @@
+@echo off
+echo Start
+winget upgrade
+echo  winget upgrade: Finished
+winget upgrade --all
+echo  winget upgrade --all: Finished
+echo  Finished upgrade
+
+chkdsk
+DISM /Online /Commit-Image /Remount-Wim /RestoreHealth
+
+cls
+echo  All Finished
